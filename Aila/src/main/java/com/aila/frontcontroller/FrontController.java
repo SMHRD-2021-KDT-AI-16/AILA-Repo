860c1b79ec3ec4d76d9928920369be7d8ba6a4e2
AILA-Repo <user@192.168.219.110>
2024-03-18 15:36:27 +0900

    지승 - frontController 수정

--- Aila/src/main/java/com/aila/frontcontroller/FrontController.java
+++ Aila/src/main/java/com/aila/frontcontroller/FrontController.java
@@ -1,6 +1,8 @@
 package com.aila.frontcontroller;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +10,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.aila.controller.command;
+
 @WebServlet("*.do")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
- 
-
+	
+	HashMap<String, command> map = new HashMap<>();
+	@Override
 	public void init(ServletConfig config) throws ServletException {
+		super.init();
 		
 	}
 
