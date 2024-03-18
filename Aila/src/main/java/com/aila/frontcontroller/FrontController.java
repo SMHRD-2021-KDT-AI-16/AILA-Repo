af85c7588941312c3d714ef2fd7d6529c47fcd24
user <user@192.168.219.103>
2024-03-18 14:30:05 +0900

    재횬쓰커밋

--- Aila/src/main/java/com/aila/frontcontroller/FrontController.java
+++ Aila/src/main/java/com/aila/frontcontroller/FrontController.java
@@ -1,6 +1,8 @@
 package com.aila.frontcontroller;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,12 +10,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.aila.controller.command;
+
 @WebServlet("*.do")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
- 
+	
+	// String과 command 타입을 가지는 HashMap 객체를 생성합니다.
+	HashMap<String, command> map = new HashMap<>();
 
 	public void init(ServletConfig config) throws ServletException {
+		super.init();
+		
 		
 	}
 
---
+++ Aila/src/main/java/com/aila/model/DeepAnalysis.java
@@ -0,0 +1,31 @@
+package com.aila.model;
+
+public class DeepAnalysis {
+	
+	private int deep_idx;
+	private String deep_result;
+	private int deep_date;
+	
+	
+	public int getDeep_idx() {
+		return deep_idx;
+	}
+	public void setDeep_idx(int deep_idx) {
+		this.deep_idx = deep_idx;
+	}
+	public String getDeep_result() {
+		return deep_result;
+	}
+	public void setDeep_result(String deep_result) {
+		this.deep_result = deep_result;
+	}
+	public int getDeep_date() {
+		return deep_date;
+	}
+	public void setDeep_date(int deep_date) {
+		this.deep_date = deep_date;
+	}
+	
+	
+	
+}
